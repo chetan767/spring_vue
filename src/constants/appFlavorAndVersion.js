58564868f8eeae4350339c745973ea692dae160e
chetan767 <chetankhanna767@gmail.com>
2024-12-04 23:59:55 +0530

    dev flavour added

--- src/constants/appFlavorAndVersion.js
+++ src/constants/appFlavorAndVersion.js
@@ -6,6 +6,6 @@ const FlavorType = {
 
 const APP_VERSION = "1";
 const APP_BUILDNUMBER = "1";
-const APP_FLAVOR = FlavorType.LOCAL;
+const APP_FLAVOR = FlavorType.DEV;
 
 export default { FlavorType, APP_VERSION, APP_BUILDNUMBER, APP_FLAVOR };
