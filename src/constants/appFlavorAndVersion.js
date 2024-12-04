e35c26c5a1fe7f7308519b1dbcd0aee8a6a76d7b
chetan767 <chetankhanna767@gmail.com>
2024-12-05 01:51:03 +0530

    loader added

--- src/constants/appFlavorAndVersion.js
+++ src/constants/appFlavorAndVersion.js
@@ -6,6 +6,6 @@ const FlavorType = {
 
 const APP_VERSION = "1";
 const APP_BUILDNUMBER = "1";
-const APP_FLAVOR = FlavorType.DEV;
+const APP_FLAVOR = FlavorType.DEV; // use envrionment variable here
 
 export default { FlavorType, APP_VERSION, APP_BUILDNUMBER, APP_FLAVOR };
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { getApiBaseUrl } from "src/constants/flavour";
+import { Loading } from "quasar";
 
 const headers = {};
 
 export async function springPostAPI(path, data) {
+  Loading.show();
   return axios
     .post(
       getApiBaseUrl() + path,
@@ -13,35 +15,42 @@ export async function springPostAPI(path, data) {
       }
     )
     .then((res) => {
-      if (res.data.success == false) {
+      if (res.data.error) {
+        Loading.hide();
         return { error: true };
       }
+      Loading.hide();
       return res.data;
     })
     .catch((err) => {
       console.log(err);
+      Loading.hide();
       return { error: true };
     });
 }
 
 export async function springGetAPI(path) {
   console.log(getApiBaseUrl() + path);
-
+  Loading.show();
   return axios
     .get(getApiBaseUrl() + path)
     .then((res) => {
-      if (res.data.success == false) {
+      if (res.data.error) {
+        Loading.hide();
         return { error: true };
       }
+      Loading.hide();
       return res.data;
     })
     .catch((err) => {
+      Loading.hide();
       console.log(err);
       return { error: true };
     });
 }
 
 export async function springPatchAPI(path, data) {
+  Loading.show();
   return axios
     .patch(
       getApiBaseUrl() + path,
@@ -51,28 +60,35 @@ export async function springPatchAPI(path, data) {
       }
     )
     .then((res) => {
-      if (res.data.success == false) {
+      if (res.data.error) {
+        Loading.hide();
         return { error: true };
       }
+      Loading.hide();
       return res.data;
     })
     .catch((err) => {
       console.log(err);
+      Loading.hide();
       return { error: true };
     });
 }
 
 export async function springDeleteAPI(path, data) {
+  Loading.show();
   return axios
     .delete(getApiBaseUrl() + path)
     .then((res) => {
       if (res.data.error) {
+        Loading.hide();
         return { error: true };
       }
+      Loading.hide();
       return res.data;
     })
     .catch((err) => {
       console.log(err);
+      Loading.hide();
       return { error: true };
     });
 }
